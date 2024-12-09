2015aaa5ea9808beada32164eb0e3e0135c6ca22
Evgenii Kirillov <477504@gmail.com>
2024-12-09 18:59:18 +0400

    Initial Commit

---
+++ Amidi/AmidiApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmidiApp.swift
+//  Amidi
+//
+//  Created by Евгений on 09/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct AmidiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
